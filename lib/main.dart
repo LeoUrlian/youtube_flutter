386e74d05455d47c282256c0c73e08328ade63f1
Leonardo Sudati <leourlian@hotmail.com>
2020-04-24 15:39:49 -0300

    tirar-faixa

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       title: 'FlutterTube',
       home: Home(),
     );
